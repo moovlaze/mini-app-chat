1d439ca27506ecfe0c97a85c4b997bf6fd138cc7
moovlaze <max.moovlaze@gmail.com>
2025-06-03 18:17:32 +0300

    add model for users

---
+++ src/users/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import mapped_column, Mapped
+
+from db.base import Base
+
+class User(Base):
+	__tablename__ = "users"
+	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+	name: Mapped[str] = mapped_column(nullable=False)
+	email: Mapped[str] = mapped_column(nullable=False, unique=True)
+	password: Mapped[str] = mapped_column(nullable=False)
