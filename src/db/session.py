9f3db3434dbc987501cb609596fd6bac19b95f41
moovlaze <max.moovlaze@gmail.com>
2025-06-08 16:00:01 +0300

    change

--- src/db/session.py
+++ src/db/session.py
@@ -1,26 +1,33 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from .base import Base
-from users.models import User
-from chats.models import PersonalChat
+from users.models import Users
+from chats.models import Chats, ChatType
+from settings import Settings
 
-engine  = create_async_engine("postgresql+asyncpg://postgres:postgres@localhost:5432/postgres", echo=True)
+engine = create_async_engine(Settings.db_url, echo=True)
 
 async_session = async_sessionmaker(bind=engine)
 
+
 async def init_db():
-	async with engine.begin() as conn:
-		await conn.run_sync(Base.metadata.drop_all)
-		await conn.run_sync(Base.metadata.create_all)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
-	async with async_session() as session:
-		test_user = User(name="Jhon", email="jhon@example.com", password="securepassword")
-		test_user_two = User(name="Jimmy", email="jimmy@example.com", password="supersecurepassword")
-		print("test_user.id",test_user.id)
-		personal_chat = PersonalChat(user_id=test_user.id, two_user_id=test_user_two.id)
+    async with async_session() as session:
+        test_user = Users(
+            name="Jhon", email="jhon@example.com", password="securepassword"
+        )
+        test_user_two = Users(
+            name="Jimmy", email="jimmy@example.com", password="supersecurepassword"
+        )
+        chat_one = Chats(title="First Chat", type_chat=ChatType.personal)
+        chat_two = Chats(title="Second Chat", type_chat=ChatType.public)
 
-		session.add(personal_chat)
-		session.add(test_user)
-		session.add(test_user_two)
-		
-		await session.commit()
+        session.add(test_user)
+        session.add(test_user_two)
+        session.add(chat_one)
+        session.add(chat_two)
+
+        await session.commit()
