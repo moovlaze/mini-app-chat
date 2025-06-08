715580c065973aa9ce49f0678e23f3dc5edc0ca3
moovlaze <max.moovlaze@gmail.com>
2025-06-08 15:56:26 +0300

    add TimesTampedMixin

--- src/db/base.py
+++ src/db/base.py
@@ -1,6 +1,18 @@
-from sqlalchemy.orm import DeclarativeBase
+import datetime
+
+from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 
 class Base(DeclarativeBase):
     def __repr__(self):
         return f"{self.__class__}: | {self.__dict__}"
+
+
+class TimesTamped:
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('UTC', now())")
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('UTC', now())"), onupdate=datetime.datetime.utcnow
+    )
