9ab3ae67c719cdf95c51d9c7bbc8a1e62819c788
moovlaze <max.moovlaze@gmail.com>
2025-06-08 15:59:38 +0300

    add TimesTampedMixin

--- src/users/models.py
+++ src/users/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import mapped_column, Mapped
 
-from db.base import Base
+from db.base import Base, TimesTampedMixin
 
-class User(Base):
-	__tablename__ = "users"
-	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-	name: Mapped[str] = mapped_column(nullable=False)
-	email: Mapped[str] = mapped_column(nullable=False, unique=True)
-	password: Mapped[str] = mapped_column(nullable=False)
+
+class Users(Base, TimesTampedMixin):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
