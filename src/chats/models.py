3277d195555134aff62247598a3eb1709eb5af9e
moovlaze <max.moovlaze@gmail.com>
2025-06-08 15:59:02 +0300

    change structure db

--- src/chats/models.py
+++ src/chats/models.py
@@ -1,8 +1,26 @@
-from db.base import Base
-from sqlalchemy.orm import mapped_column, Mapped
+import enum
 
-class PersonalChat(Base):
-	__tablename__ = "personal_chats"
-	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-	user_id: Mapped[int] = mapped_column(nullable=False)
-	two_user_id: Mapped[int] = mapped_column(nullable=False)
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import ForeignKey
+
+from db.base import Base, TimesTampedMixin
+
+
+class ChatType(enum.Enum):
+    personal = "Personal"
+    public = "Public"
+
+
+class Chats(Base, TimesTampedMixin):
+    __tablename__ = "chats"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str | None]
+    type_chat: Mapped[ChatType]
+
+
+class UserToChats(Base):
+    __tablename__ = "user_to_chat"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id", ondelete="CASCADE"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
