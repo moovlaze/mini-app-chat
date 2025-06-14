e784e0608328fddeff2b25c18f6a8aac92d97fdb
moovlaze <max.moovlaze@gmail.com>
2025-06-14 23:28:15 +0300

    add division views files and add func save_message

--- src/views.py
+++ src/views.py
@@ -1,39 +1,22 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from session import async_session
+from models import User
 
 router = APIRouter()
 
-class ConnectionManager:
-	def __init__(self):
-		self.active_connections: list[WebSocket] = []
+class CreateUserSchema(BaseModel):
+	name: str
+	email: str
 
-	async def connect(self, websocket: WebSocket):
-		await websocket.accept()
-		self.active_connections.append(websocket)
-
-	def disconnect(self, websocket: WebSocket):
-		self.active_connections.remove(websocket)
-
-	async def send_personal_message(self, message: dict, websocket: WebSocket):
-		await websocket.send_json(message)
-
-	async def broadcast(self, message: dict, websocket: WebSocket):
-		for connection in self.active_connections:
-			if connection != websocket:
-				await connection.send_json(message)
-
-
-manager = ConnectionManager()
-
-@router.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-	await manager.connect(websocket)
-	try:
-		while True:
-			data = await websocket.receive_json()
-			print(type(data))
-			await manager.send_personal_message(data, websocket)
-			await manager.broadcast(data, websocket)
-	except WebSocketDisconnect:
-		manager.disconnect(websocket)
-		await manager.broadcast(f"Client #{client_id} left the chat")
+@router.post("/users/")
+async def create_user(data: CreateUserSchema):
+	async with async_session() as session:
+		user = User(name=data.name, email=data.email)
+		
+		session.add(user)
+		await session.commit()
   
+	return {"message": "create user"}
+
---
+++ src/websocket_views.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+from session import async_session
+from models import User, Chat, UserToChat, Message
+
+router = APIRouter()
+
+
+async def create_chat(user_id: int, chat_id: int):
+    async with async_session() as session:
+        pass
+    
+    
+async def save_message(data: dict):
+    print(f"{data=}")
+    print(f"{data['sender_id']}")
+    async with async_session() as session:
+        message = Message(
+			sender_id=data["sender_id"],
+			chat_id=data["chat_id"],
+			text=data["text"]
+		)
+        
+        session.add(message)
+        await session.commit()
+
+class ConnectionManager:
+	def __init__(self):
+		self.active_connections: dict = {}
+
+	async def connect(self, websocket: WebSocket, chat_id: int):
+		await websocket.accept()
+		if chat_id in self.active_connections:
+			self.active_connections[chat_id]["connections"].append(websocket)
+		else:
+			self.active_connections[chat_id] = {"connections": [websocket]}
+
+	def disconnect(self, websocket: WebSocket, chat_id: int):
+		self.active_connections[chat_id]["connections"].remove(websocket)
+
+	async def send_personal_message(self, message: dict, websocket: WebSocket):
+		await websocket.send_json(message)
+
+	async def broadcast(self, data: dict, websocket: WebSocket, chat_id: int):
+		for connection in self.active_connections[chat_id]["connections"]:
+			if connection != websocket:
+				await save_message(data)
+				await connection.send_json(data)
+
+
+manager = ConnectionManager()
+
+@router.websocket("/ws/{user_id}/{chat_id}")
+async def websocket_endpoint(websocket: WebSocket, user_id: int, chat_id: int):
+	await manager.connect(websocket, chat_id)
+	try:
+		while True:
+			data = await websocket.receive_json()
+			print(type(data))
+			await manager.send_personal_message(data, websocket)
+			await manager.broadcast(data, websocket, chat_id)
+	except WebSocketDisconnect:
+		manager.disconnect(websocket, chat_id)
+  
