516efeeb3860532d66c36a9ca9531de20f534aea
moovlaze <max.moovlaze@gmail.com>
2025-06-16 10:13:19 +0300

    include new router into main

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from session import init_db
-from views import router
+from websocket_views import router as websocker_router
+from views import router as views_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(websocker_router)
+app.include_router(views_router)
 		
 if __name__ == "__main__":
 	asyncio.run(init_db())
